c7133f1a6f019357c9b5f326eb5439d564bd6d0f
Dynash EU <dynasheu@users.noreply.github.com>
2018-09-08 12:51:15 +0200

    button and relay portion working

--- standby-psu-test/standby-psu-test.ino
+++ standby-psu-test/standby-psu-test.ino
@@ -1,9 +1,9 @@
-//load libraries
-#include "IRremote.h"
-#include "SendOnlySoftwareSerial.h"
-#include "avr/interrupt.h"
+// load libraries
+//#include "IRremote.h"
+//#include "SendOnlySoftwareSerial.h"
+//#include "avr/interrupt.h"
 
-//pinout
+// pinout
 #define relayPin 5
 #define ledPin 4
 #define buttonPin 2
@@ -13,24 +13,24 @@
 int ledState = HIGH;        // state of standby led. turned HIGH when system is off
 int powerState = LOW;       // power state, LOW when system is off
 
-int debounceDelay = 100;                        // debounce time
+int debounceDelay = 50;                        // debounce time
 unsigned long powerStateTimer = 0;              // time snapshot when power state changed
 long powerStateDelay = 15000;                   // power state delay time
 
 // IR setup
-IRrecv irrecv(irPin);
-decode_results results;
+//IRrecv irrecv(irPin);
+//decode_results results;
 
 // Software serial
 //SendOnlySoftwareSerial mySerial (serialOutPin);       // Tx pin
 
 void setup() {
-    //serial setup
+    // serial setup
     //mySerial.begin (9600);
-    Serial.begin (9600);
-    digitalWrite(serialOutPin, HIGH);   //test
+    Serial.begin (115200);
+//    digitalWrite(serialOutPin, HIGH);   //test
 
-    //pinout setup
+    // pinout setup
     pinMode(buttonPin, INPUT_PULLUP);
     pinMode(ledPin, OUTPUT);
     pinMode(irPin, INPUT);
@@ -39,52 +39,101 @@ void setup() {
     //attachInterrupt(digitalPinToInterrupt(buttonPin), btnDown, CHANGE);
     //attachInterrupt(digitalPinToInterrupt(buttonPin), btnUp, RISING);
 
-    //defaults
+    // defaults
     digitalWrite(ledPin, ledState);
     digitalWrite(relayPin, powerState);
 
-    //ir setup
-    irrecv.enableIRIn();
+    // ir setup
+    //irrecv.enableIRIn();
 }
 
 void loop() {
-    //startup delay and powerdown blinking led delay
+    // startup delay and powerdown blinking led delay
     while (millis() - powerStateTimer < powerStateDelay) {
         digitalWrite(ledPin, HIGH);
-        delay(50);
+        delay(debounceDelay);
         digitalWrite(ledPin, LOW);
-        delay(950);
+        delay(debounceDelay * 19);
     }
-    //reset led to correct state
+    // reset led to correct state
     digitalWrite(ledPin, ledState);
 
+    // read ir
+    // if (irrecv.decode(&results)) {
+    //     if (results.value != 4294967295) {
+    //         Serial.print ("Read code: ");
+    //         Serial.println (results.value);
+    //     }
+    //     if (results.value == 18556633) {
+    //         digitalWrite(serialOutPin, HIGH);    //test
+    //     }
+    //     irrecv.resume(); // Receive the next value
+    // }
 
-    if (irrecv.decode(&results)) {
-        if (results.value != 4294967295) {
-            Serial.print ("Read code: ");
-            Serial.println (results.value);
-        }
-        if (results.value == 18556633) {
-            digitalWrite(serialOutPin, HIGH);    //test
-        }
-        irrecv.resume(); // Receive the next value
+    // read button
+    int buttonPress = 0;
+    while (digitalRead(buttonPin) == LOW && buttonPress < 30) {
+        buttonPress++;
+        delay(debounceDelay);
+        Serial.print ("Button pressed for (ms): ");
+        Serial.println (buttonPress * debounceDelay);
     }
 
+    if (buttonPress > 0) {
+        Serial.println ("--");
+    }
+
+    if (buttonPress > 29) {
+        // power on or off
+        if (powerState == HIGH) {
+            // turning off
+            delay(debounceDelay);
+            ledState = powerState;
+            powerState = !powerState;
+            powerStateTimer = millis();
+
+            // write to digital pins
+            digitalWrite(ledPin, ledState);
+            digitalWrite(relayPin, powerState);
+        }
+        else if (powerState == LOW) {
+            // turning on
+            delay(debounceDelay);
+            ledState = powerState;
+            powerState = !powerState;
+            powerStateTimer = millis();
+
+            // write to digital pins
+            digitalWrite(ledPin, ledState);
+            digitalWrite(relayPin, powerState);
+        }
+    }
+    else if (buttonPress > 1 && buttonPress < 20 && powerState == HIGH) {
+        // send short press signal over serial
+        digitalWrite(serialOutPin, HIGH);
+        delay(debounceDelay * 2);
+        digitalWrite(serialOutPin, LOW);
+    }
+    // Serial.print ("Power state: ");
+    // Serial.print (powerState);
+    // Serial.print ("    Led state: ");
+    // Serial.print (ledState);
+    // Serial.print ("    Power state timer: ");
+    // Serial.print (powerStateTimer);
+    // Serial.print ("    Button state: ");
+    // Serial.println (digitalRead(buttonPin));
     
-    //read button
-    //read ir
-    //set relay
-    //send serial data
+
 }
 
-void btnDown() {
-    Serial.println ("button down sub");
-    digitalWrite(serialOutPin, LOW);
-    delay(debounceDelay);
-}
+// void btnDown() {
+//     Serial.println ("button down sub");
+//     digitalWrite(serialOutPin, LOW);
+//     delay(debounceDelay);
+// }
 
-void btnUp() {
-    Serial.println ("button up sub");
-    digitalWrite(serialOutPin, HIGH);
-    delay(debounceDelay);
-}
+// void btnUp() {
+//     Serial.println ("button up sub");
+//     digitalWrite(serialOutPin, HIGH);
+//     delay(debounceDelay);
+// }
